51d954a68210546cc676c37567f3121bcb1b1bb5
Marcus Lindblom <m@marcuslindblom.com>
2024-09-20 16:24:29 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://marcuslindblom.github.io',
+  base: 'anchorman-site',
+  integrations: [svelte()],
+  vite: {
+    ssr: {
+      noExternal: ['@strifeapp/strife', '@strifeapp/image', '@strifeapp/picture']
+    }
+  },
+});
---
+++ src/components/Heading.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import { subscribe } from "@strifeapp/strife";
+
+  export let heading = 'Somebody...';
+
+  let unsubscribe: () => void;
+
+  onMount(() => {
+    unsubscribe = subscribe((data) => {
+      heading = data.heading
+    });
+  });
+
+  onDestroy(() => {
+    if (unsubscribe) {
+      unsubscribe();
+    }
+  });
+</script>
+
+<h1>{heading}</h1>
